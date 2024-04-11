faf70b9b5133275bc10fe1aa973164b3c8e812d4
Manjot Kaur <manjotkaur@Manjots-MacBook-Pro.local>
2024-04-11 11:48:27 -0400

    Complete

---
+++ adesh-bmi.js
@@ -0,0 +1,17 @@
+let weight = 90;
+let height = 1.91;
+
+
+function calculateBMI(){
+ interpretBMI  = weight / (height/100) * 2;
+ if (interpretBMI < 18.5){
+    console.log("underweight")
+ } else if(interpretBMI  > 18.5 && interpretBMI < 25){
+   console.log("Normal weight")
+ } else if( interpretBMI > 25 && interpretBMI < 30){
+    console.log("Overweight")
+ } else{
+    console.log("Obese")
+ }
+}
+console.log(interpretBMI);
